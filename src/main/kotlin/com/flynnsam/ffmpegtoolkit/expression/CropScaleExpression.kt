d70cb3f8e6cfcd6b433e6d6560c20e38529b4cda
Sam Flynn <flynn.samuel@gmail.com>
2018-07-15 17:14:06 -0400

    Updates to ignore and crop expression

--- src/main/kotlin/com/flynnsam/ffmpegtoolkit/expression/CropScaleExpression.kt
+++ src/main/kotlin/com/flynnsam/ffmpegtoolkit/expression/CropScaleExpression.kt
@@ -3,6 +3,7 @@ package com.flynnsam.ffmpegtoolkit.expression
 class CropScaleExpression {
 
     companion object {
-        const val INPUT_VIDEO_WIDTH : Symbol = () -> ""
+        const val INPUT_VIDEO_WIDTH : Symbol = () -> "iw"
+
     }
 }
